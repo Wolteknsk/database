fe5d3736a7ea266bc02a5aaa342326aa0869d510
Wolteknsk <andsam913@gmail.com>
2026-03-11 17:40:51 +0700

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS simple_auth;
+USE simple_auth;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
